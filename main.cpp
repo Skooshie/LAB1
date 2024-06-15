f0c5fa3289b5617e96c0935df4fd87eafdde2af3
Skooshie <james.mcenaney00@gmail.com>
2024-06-15 15:01:35 +0000

    LAB1OK

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include "mbed.h"
+#include <cstdio>
+//* Setup Digital Input to Detect Button Press*//
+
+
+InterruptIn bluebtn(PC_13);
+volatile int int_trig = 0;
+
+
+void bttnFall(){
+    int_trig = 1;
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+// Setup the on board leds//
 
+    DigitalOut led(LED1);
+    DigitalOut led2(LED2);
+
+    bluebtn.fall(&bttnFall);
+
+    // This loop looks at the input and sends the output to print and the leds
+    // The button press turns the led's on and off
+    while (true) {
+        if(int_trig == 1){
+            int_trig = 0;
+            printf("Blue Button Pressed \r\n");
+           led = !led;
+           led2 = !led2;
+          
+        }
     }
 }
-
